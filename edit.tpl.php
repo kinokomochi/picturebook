f8ba73ea83f9e7a8858d7f42df5e84ff2a3e00c1
kinokomochi <toadstomochi@gmail.com>
2020-02-28 15:23:03 +0900

    add validation

--- edit.tpl.php
+++ edit.tpl.php
@@ -1,37 +1,46 @@
 <!DOCTYPE html>
 <html>
 <?php include('header_inc.php') ?>
+<?php include('function.php') ?>
 <body>
 <h2><?=$message ; ?></h2>
     <p></p>
 
-    <p>[写真]:<?=$pbook['picture']."\n<br>" ?></p>
-   
+    <p>[写真]:<img src="files/<?php h($pbook['picture']); ?>" 
+        width="300" height="300" alt="" /> <br></p>  
 <form action="update.php" method="post" enctype="multipart/form-data">
     <input type="hidden" name="id" value="<?=$id ?>" > 
+    <input type="hidden" name="picture" value="files/<?php $pbook['picture'] ?>" > 
 
     <label>種名</label>
-    <input type="text" name="sp_name" value="<?php echo $pbook['sp_name'] ?>">
-    <label>班を選んでね</label>
+    <input type="text" name="sp_name" value="<?php echo $pbook['sp_name']; ?>">
+    <?php if(isset($error['sp_name']) && $error['sp_name'] == 'blank'): ?>
+    <p>*種名を登録してください*</p>
+    <?php endif; ?>
+    <?php if(isset($error['sp_name']) && $error['sp_name'] == 'length'): ?>
+    <p>*種名は５０文字以内で登録してください*</p>
+    <?php endif; ?>
+    
+    <label>班</label>
     <select name="team" value="<?php echo $pbook['team'] ?>">
-    <option value="sea">海</option>
-    <option value="kinoko">きのこ</option>
-    <option value="plant">植物</option>
+    <option value="">選んでね</option>
+    <option value="sea"<?php if(isset($pbook['team']) && $pbook['team'] == 'sea'){echo 'selected';} ?>>海</option>
+    <option value="kinoko"<?php if(isset($pbook['team']) && $pbook['team'] == 'kinoko'){echo 'selected';} ?>>きのこ</option>
+    <option value="plant"<?php if(isset($pbook['team']) && $pbook['team'] == 'plant'){echo 'selected';} ?>>植物</option>
     </select>
+    <?php if(isset($error['team']) && $error['team'] == 'blank'): ?>
+    <p>*班を選んでください*</p>
+    <?php endif; ?>
     <p></p>
     <label>説明</label>
-    <textarea name="description" cols="40" row="80"><?php echo $pbook['description'] ?></textarea>
+    <textarea name="description" cols="40" row="80"><?php echo $pbook['description']; ?></textarea>
+    <?php if(isset($error['description']) && $error['description'] == 'blank'): ?>
+    <p>*説明を登録してください*</p>
+    <?php endif; ?>
     <p></p>
     <input type="submit" name="submit" value="更新する">
     </form>
     <p><a href="room.php">班一覧に戻る</a></p>
-
-
- <?php //var_dump($pbook); ?>
-
-
-
-
 
 <?php include('footer_inc.php') ?>
 </body>
--- update.php
+++ update.php
@@ -1,40 +1,110 @@
 <?php 
+error_reporting(E_ALL);
+require_once ('db_connect.php');
+
+$message = "入力エラーがあります";
     if(isset($_POST['submit'])){
-        $id = $_POST['id'];
-        $team = $_POST['team'];
-        $sp_name = $_POST['sp_name'];
-        $description = $_POST['description'];
+        $id = isset($_POST['id']) ? $_POST['id'] : '';
+        $team = isset($_POST['team']) ? $_POST['team'] : '';
+        $sp_name = isset($_POST['sp_name']) ? $_POST['sp_name'] : '';
+        $description = isset($_POST['description']) ? $_POST['description'] : '';
 
+        $sql = 'SELECT picture FROM picture WHERE id=:id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $picture = $stmt->fetch(PDO::FETCH_ASSOC);
+        $picture = $picture['picture'];
+        // $pdo =null;
+        // $stmt = null;
+        if($id == ''){
+            header('Location: room.php');
+        }
+        if($sp_name == ''){
+            $error['sp_name'] = 'blank';
+        }
+        //種名が５０文字以上だったら$error配列に'length'を入れる
+        if(mb_strlen($sp_name) > 50){
+            $error['sp_name'] = 'length';
+        }
+        //班欄が空欄だったら$error配列に'blank'を入れる
+        if($team == ''){
+            $error['team'] = 'blank';
+        }
+        //説明欄が空欄だったら$error配列に'blank'を入れる
+        if($description == ''){
+            $error['description'] = 'blank';
+        }
+        //$error配列に値がセットされているかどうか調べる
+        //セットされていればedit.tpl.phpを呼び出す
+        //この時インターフェイスは$error, ?
+        $pbook['picture'] = $picture;
+        $pbook['sp_name'] = $sp_name;
+        $pbook['team'] = $team;
+        $pbook['description'] = $description;
+        if(isset($error)){
+            require_once ('edit.tpl.php');
+        }
+        //$errorに値が一つも入っていなければDBに接続する
+        if(!isset($error)){
+        $sql = 'UPDATE picture 
+                SET sp_name = :sp_name, team = :team, description = :description
+                WHERE picture.id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':sp_name', $sp_name, PDO::PARAM_STR);
+        $stmt->bindValue(':team', $team, PDO::PARAM_STR);
+        $stmt->bindValue(':description', $description, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $pdo =null;
+        $stmt = null;
+        }
     }
-    // var_dump($_POST);
-    // echo $_POST['id']."\n";
-    // echo $_POST['sp_name']."\n";
-    // echo $_POST['team']."\n";
-    // echo $_POST['description']."\n";
-    // echo $id;
-    echo $sp_name;
-    require_once ('db_connect.php');
-    $sql = 'UPDATE picture 
-            SET sp_name = :sp_name, team = :team, description = :description
-            WHERE picture.id = :id';
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-    $stmt->bindValue(':sp_name', $sp_name, PDO::PARAM_STR);
-    $stmt->bindValue(':team', $team, PDO::PARAM_STR);
-    $stmt->bindValue(':description', $description, PDO::PARAM_STR);
-    $stmt->execute();
 
-    
-    
-    
-    $pdo =null;
-    $stmt = null;
+require_once __DIR__ . '/vendor/autoload.php';
 
-    
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
 
-    //echo $team;
+date_default_timezone_set("Asia/Tokyo");
+
+//ログファイルのパス
+$logging_path = __DIR__ . '/log/update_log.log';
+$stream = new StreamHandler($logging_path, Logger::INFO);
+//出力後、改行するために下記クラスを静止し、パラメーターとしてセットする。
+$formatter = new LineFormatter(null, null, true);
+$stream->setFormatter($formatter);
+$logger = new Logger('pbook/update.php');
+$logger->pushHandler($stream);
+
+//下記のようにしないと配列などの値が出力されない
+$logger->pushProcessor(function($record){
+    $record['extra']['dummy'] = 'hello world';
+    return $record;
+});
+
+//$arrは出力したいデータ
+$logger->addInfo('$pbook:' . dumper($pbook));
+$logger->addDebug('$_POST:'.var_export($_POST,true));
+$logger->warning('$_picture:'.var_export($picture,true));
+$logger->error('エラーメッセージ');
+
+//var_dumpの結果を文字列として出力するために下記関数を追加
+function dumper($obj){
+    ob_start();//関数の出力のバッファリングをオンにする　？
+    var_dump($obj);
+    $ret = ob_get_contents();//文字列変数にバッファした内容をコピー
+    ob_end_clean();//バッファの内容を消去
+    return $ret;
+}
+
+   
+    if(empty($error)){ 
     $url = "{$team}/index.php";
     header('Location:'.$url);
     exit();
+    }
 
     
