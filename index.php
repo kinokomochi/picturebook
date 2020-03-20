f8170ce328ae616d323f2a1a5bffbc9ded4d1f25
kinokomochi <toadstomochi@gmail.com>
2020-03-20 14:21:23 +0900

    made new index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once('init.php');
+$pdo = connectDB();
+$pbooks = findAllPbook($pdo,$_GET['team']);
+logD(count($pbooks), 'index');
+if($_GET['team'] == "kinoko"){
+    $message = "きのこの部屋";
+}elseif($_GET['team'] == "sea"){
+    $message = "海の部屋";
+}elseif($_GET['team'] == "plant"){
+    $message = "植物の部屋";
+}
+require_once('index.tpl.php');
+// /var_dump($_GET);
+//var_dump($pbook);
