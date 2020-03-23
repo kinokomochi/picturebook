f0549fefa978adb4353645578f452355e549e314
kinokomochi <toadstomochi@gmail.com>
2020-03-23 16:44:41 +0900

    fixed

--- new.php
+++ new.php
@@ -1,16 +1,16 @@
 <?php 
 session_start();
-require_once('function.php');
+require_once __DIR__ . '/vendor/autoload.php';
+require_once('init.php');
 $pbook = ['id'=>'', 'sp_name'=>'', 'picture'=>'', 'description'=>'',
           'team'=>'', 'user_id'=>''];
-var_dump($_SESSION);
-if(!isset($_SESSION['id'])){
+if(($_SESSION['time'] + 3600) <= time()){
     $_SESSION['return_uri'] =  "http://localhost/pbook/new.php";
     header('Location:login/login.php');
     exit;
 }
 
-if(isset($_SESSION['id']) && ($_SESSION['time']) + 3600 > time()){
+if(isset($_SESSION['id']) && ($_SESSION['time'] + 3600) > time()){
 
 $message = "図鑑登録";
 require_once ('new.tpl.php');
