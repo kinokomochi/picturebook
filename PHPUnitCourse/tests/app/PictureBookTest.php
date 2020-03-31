d9cbc77b0ab142977ac8dc9b8e4f32e713b4d7a4
kinokomochi <toadstomochi@gmail.com>
2020-03-31 11:28:19 +0900

    fixed path

--- PHPUnitCourse/tests/app/PictureBookTest.php
+++ PHPUnitCourse/tests/app/PictureBookTest.php
@@ -1,6 +1,6 @@
 <?php
 require_once('vendor/autoload.php');
-
+require_once('PHPUnitCourse/src/app/PictureBook.php');
 class PictureBookTest extends PHPUnit\Framework\TestCase {
     public function test_add() {
         $sample = new PictureBook\PictureBook();
