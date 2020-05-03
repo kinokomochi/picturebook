acd1e01a4dbada031be0d03b4c461640ad0e9176
kinokomochi <toadstomochi@gmail.com>
2020-05-03 21:24:17 +0900

    fixed

--- login/signup_check.php
+++ login/signup_check.php
@@ -1,7 +1,6 @@
 <?php 
 session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
-var_dump($_SESSION);
 if($_SERVER['REQUEST_METHOD'] != 'POST'){
     header('Location:../room.php');
     exit;
